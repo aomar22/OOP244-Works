e182d64360eaf3c6bce509e1064ed578eb09f27e
aomar22 <aomar22@myseneca.ca>
2024-05-16 12:49:53 -0400

    Updating the reflection

--- WS01/lab/ShoppingRec.cpp
+++ WS01/lab/ShoppingRec.cpp
@@ -5,23 +5,22 @@ my professor provided to complete my workshops and assignments.*/
 #include "ShoppingRec.h"
 #include "ShoppingList.h"
 #include "Utils.h"
+    ShoppingRec getShoppingRec() {
+        ShoppingRec R = {};
+        cout << "Item name: ";
+        readCstr(R.m_title, MAX_TITLE_LENGTH);
+        cout << "Quantity: ";
+        R.m_quantity = readInt(1, MAX_QUANTITY_VALUE);
+        return R;
+    }
+    void displayShoppingRec(const ShoppingRec* shp) {
+        cout << "[" << (shp->m_bought ? 'X' : ' ') << "]" << shp->m_title <<
+            " qty:(" << shp->m_quantity << ")" << endl;
+    }
+    void toggleBoughtFlag(ShoppingRec* rec) {
+        rec->m_bought = !rec->m_bought;
 
-ShoppingRec getShoppingRec() {
-    ShoppingRec R = {};
-    cout << "Item name: ";
-    readCstr(R.m_title, MAX_TITLE_LENGTH);
-    cout << "Quantity: ";
-    R.m_quantity = readInt(1, MAX_QUANTITY_VALUE);
-    return R;
-}
-void displayShoppingRec(const ShoppingRec* shp) {
-    cout << "[" << (shp->m_bought ? 'X' : ' ') << "]" << shp->m_title <<
-        " qty:(" << shp->m_quantity << ")" << endl;
-}
-void toggleBoughtFlag(ShoppingRec* rec) {
-    rec->m_bought = !rec->m_bought;
-
-}
-bool isShoppingRecEmpty(const ShoppingRec* shp) {
-    return shp->m_title[0] == 0;
-}
+    }
+    bool isShoppingRecEmpty(const ShoppingRec* shp) {
+        return shp->m_title[0] == 0;
+    }
--- WS01/lab/Utils.h
+++ WS01/lab/Utils.h
@@ -3,7 +3,7 @@ I have done all the coding by myself and only copied the code that
 my professor provided to complete my workshops and assignments.*/
 #ifndef SENECA_UTILS_H
 #define SENECA_UTILS_H
-using namespace std;
+//using namespace std;
 void flushkeys(void);
 bool ValidYesResponse(char ch);
 bool yes(void);
