c21a703f519cb771a0f9f6c32bb3e960637d74b3
aomar22 <aomar22@myseneca.ca>
2024-07-11 19:19:35 -0400

    MS1

--- MS1/Menu.h
+++ MS1/Menu.h
@@ -1,5 +1,23 @@
+/* Citation and Sources...
+Final Project Milestone 1
+Module: Menu
+Filename: Menu.cpp
+Version 1.0
+Author	Amany Omar
+Email: aomar22@myseneca.ca
+ID: 126 127 166
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2024/?/?  Preliminary release
+2024/?/?  Debugged DMA
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
 #ifndef SENECA_MENU_H
 #define SENECA_MENU_H
+#include <iostream>
 namespace seneca {
 	const unsigned MAX_MENU_ITEMS = 20;
 	class Menu;
