50b64a0aa60f1c4051d44d27487fd844510af60d
aomar22 <aomar22@myseneca.ca>
2024-07-18 09:40:55 -0400

    Adding README/Modifying Shap.cpp

--- WS08/lab/Shape.cpp
+++ WS08/lab/Shape.cpp
@@ -7,12 +7,12 @@ namespace seneca {
 	(using draw and getSpecs functions) so any shape object can be written or read using ostream and istream*/
 	std::ostream& Shape::operator<<(std::ostream& os)
 	{
-		// TODO: insert return statement here
+		draw(os);
 		return os;
 	}
 	std::istream& Shape::operator>>(std::istream& is)
 	{
-		// TODO: insert return statement here
+		getSpecs(is);
 		return is;
 	}
 }
