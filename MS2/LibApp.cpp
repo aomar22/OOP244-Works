5f382028a9e0aac92130649b0f560ddd1377995f
aomar22 <aomar22@myseneca.ca>
2024-07-17 20:05:45 -0400

    debugging MS2

--- MS2/LibApp.cpp
+++ MS2/LibApp.cpp
@@ -16,6 +16,7 @@ I have done all the coding by myself and only copied the code
 that my professor provided to complete my workshops and assignments.
 -----------------------------------------------------------*/
 #include <iostream>
+#include "Menu.h"
 #include "LibApp.h"
 using namespace std;
 namespace seneca {
