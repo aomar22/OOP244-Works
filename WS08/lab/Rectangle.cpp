07164fe3b2013a4f77a68e5cfc7888d59dac8354
aomar22 <aomar22@myseneca.ca>
2024-07-18 21:21:53 -0400

    w8

--- WS08/lab/Rectangle.cpp
+++ WS08/lab/Rectangle.cpp
@@ -13,7 +13,7 @@ provided to complete my project milestones and I needed help from the professor 
 
 using namespace std;
 namespace seneca {
-	Rectangle::Rectangle(const char* label, int width, int height) : LblShape(label), m_width(width), m_height(height) {
+	Rectangle::Rectangle(const char* label, int width, int height) : LblShape(label), m_height(height), m_width(width) {
 
 		if (m_height < 3 || m_width < ((int)(strlen(label)+2))) {
 			m_height = 0;
