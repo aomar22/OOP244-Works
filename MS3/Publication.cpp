ebc98b3b6f70b4b4ad7074af095e2cf20467711e
aomar22 <aomar22@myseneca.ca>
2024-07-27 20:57:20 -0400

    debugging

--- MS3/Publication.cpp
+++ MS3/Publication.cpp
@@ -55,12 +55,10 @@ namespace seneca {
 
 	bool Publication::operator==(const char* title) const
 	{
-		if (strstr(m_title, title)) {
-			return true;
-		}
-		else {
+		if (m_title == nullptr || title == nullptr) {
 			return false;
 		}
+		return (strstr(m_title, title) != nullptr);
 	}
 	Publication::operator const char* () const
 	{
@@ -73,7 +71,7 @@ namespace seneca {
 
 	bool Publication::conIO(ios& io)const
 	{
-		if (&io == &cin || &io == &cout) {
+		if (io && (&io == &cin || &io == &cout)) {
 			return true;
 		}
 		else {
@@ -130,155 +128,152 @@ namespace seneca {
 			return os;
 		}
 	}
-	istream& Publication::read(istream& istr)
-	{
-	char title[255 + 1]{};
-	char shelfId[SENECA_SHELF_ID_LEN + 1]{};
-	int membership = 0;
-	int libRef = -1;
-	Date date;
+	istream& Publication::read(istream& istr) {
+		char title[SENECA_TITLE_WIDTH + 1]{};
+		char shelfId[SENECA_SHELF_ID_LEN + 1]{};
+		int membership = 0;
+		int libRef = -1;
+		Date date;
 
-	// Clean up existing resources
-	delete[] m_title;
-	m_title = nullptr;
+		// Clean up existing resources
+		delete[] m_title;
+		m_title = nullptr;
 
-	// Reset class members
-	m_shelfId[0] = '\0';
-	m_membership = 0;
-	m_libRef = -1;
-	resetDate();
+		// Reset class members
+		m_shelfId[0] = '\0';
+		m_membership = 0;
+		m_libRef = -1;
+		resetDate();
 
-	if (conIO(istr)) {
-		// Interactive input
-		cout << "Shelf No: ";
-		istr.getline(shelfId, SENECA_SHELF_ID_LEN + 1, '\n');
-		if (strlen(shelfId) != SENECA_SHELF_ID_LEN) {
+		if (conIO(istr)) {
+			// Interactive input
+			cout << "Shelf No: ";
+			istr.getline(shelfId, SENECA_SHELF_ID_LEN + 1);
+			if (strlen(shelfId) != SENECA_SHELF_ID_LEN) {
+				istr.setstate(ios::failbit);
+			}
+
+			cout << "Title: ";
+			istr.getline(title, SENECA_TITLE_WIDTH + 1);
+
+			cout << "Date: ";
+			istr >> date;
+			if (!istr) {
+				istr.setstate(ios::failbit);
+			}
+
+			// Clear newline character remaining in the buffer
+			istr.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+		else {
+			// Non-interactive input
+			istr >> libRef;
+			istr.ignore(); // Ignore the delimiter after libRef
+			istr.getline(shelfId, SENECA_SHELF_ID_LEN + 1, '\t');
+			istr.getline(title, SENECA_TITLE_WIDTH, '\t');
+			istr >> membership;
+			istr.ignore(); // Ignore the delimiter after membership
+			istr >> date;
+
+			if (!istr) {
+				istr.setstate(ios::failbit);
+			}
+		}
+		// Validate date and handle errors
+		if (!date.validate()) {
 			istr.setstate(ios::failbit);
 		}
-
-		cout << "Title: ";
-		istr.getline(title, 255);
-
-		cout << "Date: ";
-		istr >> date;
-		if (!istr) {
-			istr.setstate(ios::failbit);
+		// If input was successful, update the object
+		if (istr) {
+			m_title = new char[strlen(title) + 1];
+			strcpy(m_title, title);
+			strcpy(m_shelfId, shelfId);
+			m_membership = membership;
+			m_libRef = libRef;
+			m_date = date;
 		}
-
-		// Clear newline character remaining in the buffer
-		istr.ignore(1000, '\n');
+		return istr;
 	}
-	else {
-		// Non-interactive input
-		istr >> libRef;
-		istr.ignore(); // Ignore the delimiter after libRef
-		istr.getline(shelfId, SENECA_SHELF_ID_LEN + 1, '\t');
-		istr.getline(title, 255, '\t');
-		istr >> membership;
-		istr.ignore(); // Ignore the delimiter after membership
-		istr >> date;
-		
-	}
-
-	// Validate date and handle errors
-	if (!date.validate()) {
-		istr.setstate(ios::failbit);
-	}
-
-	// If input was successful, update the object
-	if (istr) {
-		m_title = new char[strlen(title) + 1];
-		strcpy(m_title, title);
-		strcpy(m_shelfId, shelfId);
-		m_membership = membership;
-		m_libRef = libRef;
-		m_date = date;
-	}
-
-	return istr;
-}
 	Publication::operator bool() const
 	{
 		return m_title != nullptr && m_shelfId[0] != '\0';
 	}
 
-	Publication::Publication(const Publication& p)
-	{
-		//setEmpty();
-		m_title = nullptr;
-		m_date = Date();
-		m_membership = 0;
-		m_libRef = -1;
-		m_shelfId[0] = '\0';
-		if (this != &p) {
-			//	*this = p;
-
-			//delete[] m_title;
+		Publication::Publication(const Publication & p) {
 			m_title = nullptr;
-				if (p.m_title != nullptr) {
-			m_title = new char[strlen(p.m_title) + 1];
-			strcpy(m_title, p.m_title);
-			}
+			m_date = Date();
+			m_membership = 0;
+			m_libRef = -1;
+			m_shelfId[0] = '\0';
 
+			if (p.m_title != nullptr) {
+				m_title = new char[strlen(p.m_title) + 1];
+				strcpy(m_title, p.m_title);
+			}
 			strcpy(m_shelfId, p.m_shelfId);
 			m_membership = p.m_membership;
 			m_libRef = p.m_libRef;
 			m_date = p.m_date;
-			/*if (m_title != nullptr) {
-				m_title = new char[SENECA_TITLE_WIDTH + 1];
-				strncpy(m_title, p.m_title, SENECA_TITLE_WIDTH);
-			}
 		}
-		else {
-				m_title = nullptr;
-		}*/
-		}
-		/*else {
-			m_title = nullptr;
-		}*/
-
-	}
-
-	Publication& Publication::operator=(const Publication& p)
-	{
-		if (this != &p) {
-			m_membership = p.m_membership;
-			m_libRef = p.m_libRef;
-			m_date = p.m_date;
-			strncpy(m_shelfId, p.m_shelfId, SENECA_SHELF_ID_LEN);
-				m_shelfId[4] = '\0';
-
-				/*m_membership = p.m_membership;
-			m_libRef = p.m_libRef;
-			m_date = p.m_date;
-			m_shelfId[SENECA_SHELF_ID_LEN] = '\0';*/
-
-			if (p.m_title != nullptr) {
+		Publication& Publication::operator=(const Publication& p) {
+			if (this != &p) {
+				// Clean up existing resources
 				delete[] m_title;
-				if (p.m_title != nullptr && p.m_title[0] != '\0') {
+
+				// Copy data from p
+				m_title = nullptr;
+				if (p.m_title != nullptr) {
 					m_title = new char[strlen(p.m_title) + 1];
-					//for (int i = 0; i < SENECA_TITLE_WIDTH; i++) {
 					strcpy(m_title, p.m_title);
-					//m_title[i] = p.m_title[i];
-					//m_title = nullptr;
-				//}
 				}
-				else {
-					m_title = nullptr;
-				}
-				/*strcpy(m_shelfId, p.m_shelfId);
+				strcpy(m_shelfId, p.m_shelfId);
 				m_membership = p.m_membership;
 				m_libRef = p.m_libRef;
 				m_date = p.m_date;
-
 			}
-			else {
-				m_title = nullptr;*/
-
-			}
+			return *this;
 		}
-		return *this;
-	}
+
+	//Publication& Publication::operator=(const Publication& p)
+	//{
+	//	if (this != &p) {
+	//		m_membership = p.m_membership;
+	//		m_libRef = p.m_libRef;
+	//		m_date = p.m_date;
+	//		strncpy(m_shelfId, p.m_shelfId, SENECA_SHELF_ID_LEN);
+	//			m_shelfId[4] = '\0';
+
+	//			/*m_membership = p.m_membership;
+	//		m_libRef = p.m_libRef;
+	//		m_date = p.m_date;
+	//		m_shelfId[SENECA_SHELF_ID_LEN] = '\0';*/
+
+	//		if (p.m_title != nullptr) {
+	//			delete[] m_title;
+	//			if (p.m_title != nullptr && p.m_title[0] != '\0') {
+	//				m_title = new char[strlen(p.m_title) + 1];
+	//				//for (int i = 0; i < SENECA_TITLE_WIDTH; i++) {
+	//				strcpy(m_title, p.m_title);
+	//				//m_title[i] = p.m_title[i];
+	//				//m_title = nullptr;
+	//			//}
+	//			}
+	//			else {
+	//				m_title = nullptr;
+	//			}
+	//			/*strcpy(m_shelfId, p.m_shelfId);
+	//			m_membership = p.m_membership;
+	//			m_libRef = p.m_libRef;
+	//			m_date = p.m_date;
+
+	//		}
+	//		else {
+	//			m_title = nullptr;*/
+
+	//		}
+	//	}
+	//	return *this;
+	//}
 
 	Publication::~Publication()
 	{
