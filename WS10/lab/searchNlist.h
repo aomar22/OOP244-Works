bf253c1407dd15cb51e693a1a702e93eb26ed9d6
aomar22 <aomar22@myseneca.ca>
2024-07-30 12:26:17 -0400

    ws10

---
+++ WS10/lab/searchNlist.h
@@ -0,0 +1,23 @@
+#ifndef SENECA_SEARCHNLIST_H
+#define SENECA_SEARCHNLIST_H
+#include "Collection.h"
+/*Create a function template called search that accepts four arguments in any order you prefer:
+
+    A reference to a Collection of templated objects (template type 1). 
+    The Collection is defined in Collection.h
+    An array of templated objects; the same type as the Collection type. (template type 1)
+    Number of elements in the array of objects
+    A key templated value to search for, in the array of objects. (template type 2)
+
+The search function template returns a bool that is true only if at least one match to the key
+is found in the array of objects and false otherwise.
+
+The search function goes through all the elements of the array of objects and adds all the matches 
+found to the Collection. (use the "==" operator to check for a match between the objects and the key)*/
+namespace seneca {
+    template <typename T1, typename T2>
+    
+
+    
+}
+#endif
