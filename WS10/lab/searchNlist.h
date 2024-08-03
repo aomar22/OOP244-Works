a78428233024fd8c2d858825bce34ca4fbf27523
aomar22 <aomar22@myseneca.ca>
2024-08-02 20:46:25 -0400

    debugging WS10

--- WS10/lab/searchNlist.h
+++ WS10/lab/searchNlist.h
@@ -1,50 +1,28 @@
 #ifndef SENECA_SEARCHNLIST_H
 #define SENECA_SEARCHNLIST_H
 #include "Collection.h"
-/*Create a function template called search that accepts four arguments in any order you prefer:
 
-    A reference to a Collection of templated objects (template type 1). 
-    The Collection is defined in Collection.h
-    An array of templated objects; the same type as the Collection type. (template type 1)
-    Number of elements in the array of objects
-    A key templated value to search for, in the array of objects. (template type 2)
-
-The search function template returns a bool that is true only if at least one match to the key
-is found in the array of objects and false otherwise.
-
-The search function goes through all the elements of the array of objects and adds all the matches 
-found to the Collection. (use the "==" operator to check for a match between the objects and the key)*/
 namespace seneca {
     
-    template<typename templateType1, typename templateType2> 
-    bool search(Collection<templateType1>& collection, templateType1* objArray, int arraySize, templateType2 key);
+    template<typename templateType1, typename templateType2>
+    bool search(Collection<templateType1>& collection, templateType1 objArray[], int arraySize, templateType2 key)
+    {
+        for (int i = 0; i < arraySize; ++i) {
+            if (objArray[i] == key) {
+                collection.add(objArray[i]);
+                return true;
+            }
+        }
+        return false;
+    }
 
     template<typename templateType>
-    void listArrayElements(const char* m_title, const templateType* objects, int arrSize);
-    /*Lists all the elements of an array.
-
-Create a function template called listArrayElements that accepts three arguments in any order you prefer:
-
-    A const char* for a Title to be printed as the Title of the list.
-    A constant array of templated objects (template type)
-    The number of the elements of the array.
-
-This function first prints the Title and then goes to newline.
-
-Then it will print the row number and then insert each element of the array into cout with a newline attached.
-
-For example, if this function is called for the following array of integers.
-
-int a[]{10,20,30,40};
-
-using the Title value of "INTEGERS" and the number of elements as 4, the output would be like the following.
-
-INTEGERS
-1: 10
-2: 20
-3: 30
-4: 40
-*/
-    
+    void listArrayElements(const char* m_title, const templateType objArray[], int arraySize)
+    {
+        std::cout << m_title << std::endl;
+        for (int i = 0; i < arraySize; ++i) {
+            std::cout << i+1 << ":" << objArray[i] << std::endl;
+        }
+    }
 }
 #endif
